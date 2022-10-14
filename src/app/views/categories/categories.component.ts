df5254316c0ad457e2098b3d2643b085d72d5f0e
Dzhavid <mr.telepuzig@yandex.ru>
2022-10-14 16:00:34 +0300

    feat: добавили динамику в категориес.апп

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/model/Category';
+import { DataHandlerService } from 'src/app/service/data-handler.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  //
+  categories: Category[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dataHandler: DataHandlerService) {
+    this.categories = this.dataHandler.getCategories();
   }
 
+  // метод вызывается автоматически после инициализации компонента
+  ngOnInit(): void {
+    // console.log(this.categories);
+  }
 }
